b17755e1a748fe845c5620afc12da5932428d1c7
revanth <revanthkkompala@gmail.com>
2026-03-19 18:16:55 -0400

    Sample opMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/teleopSample.kt
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.teleop
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.normalizeDegrees
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
+
+@TeleOp(name = "teleopSample")
+
+class teleopSample : OpMode() {
+    override fun init() {
+
+    }
+    override fun loop() {}
+}
